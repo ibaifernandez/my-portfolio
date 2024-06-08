b29af0605beccab8f2e6a7654b066b2f4dbc0949
Ibai Fernández <info@ibaifernandez.com>
2024-06-08 13:44:24 -0500

    240608 Translations 78

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -890,14 +890,14 @@ Assigned to: ThemeForest
 				$(".more_content").not($(this).siblings(".more_content")).slideUp();
 				$(".ex_btn").not(this).text("Read More");
 				$(this).siblings('.more_content').slideToggle();
-			  if ($(this).text() == "Expand") {
+			  if ($(this).text() == "Read More") {
 				$(this).text("Collapse")
-			  } else if ($(this).text() == "Expandir") {
+			  } else if ($(this).text() == "Leer más") {
 				$(this).text("Contraer")
 			  } else if ($(this).text() == "Contraer") {
-				$(this).text("Expandir")
+				$(this).text("Leer más")
 			  } else if ($(this).text() == "Collapse") {
-				$(this).text("Expand")
+				$(this).text("Read More")
 			  }
 			});
 			 $('.ex_rightside').on('click', function(e) {
